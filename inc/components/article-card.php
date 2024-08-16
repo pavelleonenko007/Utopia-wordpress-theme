a54207ba6a38124803b853f2a9110b6563c5d44d
pavelleonenko007 <pavel.leonenko374@gmail.com>
2024-08-16 10:30:44 +0300

    fix: author name block in article card in concert page

--- inc/components/article-card.php
+++ inc/components/article-card.php
@@ -6,10 +6,11 @@
  */
 
 defined( 'ABSPATH' ) || exit;
+$post_author = get_field( 'author' );
 ?>
 <div id="w-node-_7cd3ecbb-0adc-fbe2-33ca-4b33d28cf0e4-f702ec0a" class="artic-item">
 	<a href="<?php the_permalink(); ?>" class="artic-link w-inline-block">
 		<div class="p-16-120 w"><?php the_title(); ?></div>
-		<div class="p-16-120">by <?php the_author(); ?></div>
+		<div class="p-16-120"><?php echo esc_html( $post_author ); ?></div>
 	</a>
 </div>
