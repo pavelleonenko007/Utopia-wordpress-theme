1fca4754c8c2472d0e1eb5e920531ce1ede8118f
pavelleonenko007 <pavel.leonenko374@gmail.com>
2024-09-17 10:57:43 +0300

    fix: concert min card html typo

--- inc/components/concert-card-min.php
+++ inc/components/concert-card-min.php
@@ -10,7 +10,7 @@ $start_date = get_field( 'start_date' );
 ?>
 <div id="w-node-_7aaefb87-e36f-2cdd-6ef1-cc91a73d7052-f702ec0a" class="conc-card min">
 	<a href="<?php the_permalink(); ?>" class="conc-link min w-inline-block">
-		<div class="p-28-120<?php echo esc_html( gmdate( 'j', $start_date ) ) . ' ' . esc_html( pll__( gmdate( 'M', $start_date ) ) ); ?></div>
+		<div class="p-28-120"><?php echo esc_html( gmdate( 'j', $start_date ) ) . ' ' . esc_html( pll__( gmdate( 'M', $start_date ) ) ); ?></div>
 		<div class="p-28-120 _2"><?php the_title(); ?></div>
 		<?php
 		$location_name = utopia_get_concert_location( get_the_ID() );
